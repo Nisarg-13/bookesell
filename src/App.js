4ada583951898e17104a186e4016275fcdbc917b
Nisarg-13 <123094651+Nisarg-13@users.noreply.github.com>
2023-08-03 09:33:08 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import './index.js';
 import { Route, Routes, NavLink } from 'react-router-dom';
 import Welcome from './components/Welcome';
 import User from './components/User';
+import {CssBaseline} from '@material-ui/core';
 import Home from './components/Home';
 import { purple } from '@material-ui/core/colors';
 import { createTheme ,ThemeProvider } from '@material-ui/core/styles';
@@ -12,7 +14,7 @@ const theme = createTheme({
       // Purple and green play nicely together.
       // main: purple[500],
       light: "#ff4081",
-      main: purple[500],
+      main: purple[500], 
       dark: "#c51162",
       contrastText: "#fff"
     },
@@ -20,23 +22,27 @@ const theme = createTheme({
       // This is green.A700 as hex.
       main: '#11cb5f',
     },
-    typography: {
-      h1: {
-        fontWeight: 300,
-        fontSize: "6rem",
-        lineHeight: 1.167,
-        letterSpacing: "-0.01562em",
-        color:"#ff4081 !important"
-      }
-    }
+  },
+  typography: {
+    fontFamily:'Fira Sans',
+    fontSize:15,
+    h1: {
+      fontWeight: 300,
+      fontSize: "6rem",
+      lineHeight: 1.167,
+      letterSpacing: "-0.01562em",
+      color:"#ff4081 !important",
+    },
+    
   },
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-
-      <div>
+      <CssBaseline />
+  
+      <div >
         <div style={
           {
             display: 'flex',
@@ -47,15 +53,15 @@ function App() {
           }
         }>
 
-            <NavLink to='/'> HOME </NavLink><br />
-            <NavLink to='/welcome'> WELCOME </NavLink><br />
-            <NavLink to='/user'> USER </NavLink>
+            <NavLink to='/' style={{fontSize:'1.2rem',textDecoration:'none'}}>HOME 🏠</NavLink><br />
+            <NavLink to='/welcome' style={{fontSize:'1.3rem',textDecoration:'none'}} > WELCOME 👋</NavLink><br />
+            <NavLink to='/user' style={{fontSize:'1.3rem',textDecoration:'none'}}> USER 🤵 </NavLink>
         </div>
 
-        <Routes>
+        <Routes path='/'>
           <Route index element={<Home/>} />
           <Route path='/welcome' element={<Welcome />} />
-          <Route path='/user' element={<User name="Nisarg" role="student" />} />
+          <Route path='/user' element={<User name="Darshan" role="student" />} />
         </Routes>
       </div>
     </ThemeProvider>
