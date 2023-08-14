30067b22d1740270823d8f61cdd72f8cc3eccbc6
Nisarg-13 <123094651+Nisarg-13@users.noreply.github.com>
2023-08-14 18:55:53 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import './index.js';
 import { Route, Routes, NavLink } from 'react-router-dom';
-import Welcome from './components/Welcome';
 import Register from './components/Register';
 import { CssBaseline, useTheme } from '@material-ui/core';
 import Home from './components/Home';
 import { purple } from '@material-ui/core/colors';
 import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import { ToastContainer, toa } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
 import Login from './components/Login';
 import Header from './components/Header';
 import { AuthProvider } from './contexts/auth';
-
-
+import { CartProvider } from './contexts/cartContext';
+import EditBook from './components/EditBook';
+import ProductPage from './components/ProductPage';
+import Users from './components/Users';
+import EditUser from './components/EditUser';
 const theme = createTheme({
   palette: {
     primary: {
@@ -50,16 +52,24 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
+      <CartProvider>  
       <Header/>
       <ToastContainer />
       <CssBaseline />
       <Routes path='/'>
         <Route index element={<Home />} />
-        <Route path='/welcome' element={<Welcome />} />
+        {/* <Route path='/welcome' element={<Welcome />} /> */}
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/editBook/:id' element={<EditBook />} />
+        <Route path='/product' element={<ProductPage />} />
+        <Route path='/add-book' element={<EditBook />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/editUser/:id' element={<EditUser />} />
+
       </Routes>
 
+      </CartProvider>
       </AuthProvider>
     </ThemeProvider>
 
